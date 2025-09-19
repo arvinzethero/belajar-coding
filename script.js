e289fdde555a5236739df491adb7a48b4b4e8b64
arvinzethero <arvinhidayat12@gmail.com>
2025-09-19 13:18:14 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Belajar menulis js
+var angka = 2;
+// Belajar popup box
+// alert("selamat datang");
+
+// var lagi = true;
+// while( lagi === true ) {
+//     var nama =  prompt("masukan nama anda");
+//     alert(`Halo nama kamu pasti ${nama}`);
+
+//     lagi = confirm("coba lagi ?")
+// }
+// alert("Terima Kasih");
+
+
+// Belajar Pengulangan
+// let i = 0;
+// while(i < 10) {
+//     console.log("Hello Word");
+//     i++;
+// }
+// alert("Selesai");
+
+let ulang = true;
+while(ulang) {
+    console.log("Hello Bitch");
+    ulang = confirm("Mau lagi ?");
+}
+alert("selesai");
