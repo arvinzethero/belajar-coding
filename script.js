e1f5d844ae3014fd18b48867028ff066329a35a5
arvinzethero <arvinhidayat12@gmail.com>
2025-09-29 13:51:03 +0700

    tahu sakit

--- script.js
+++ script.js
@@ -65,10 +65,10 @@
 var jmlAngkot = 10;
 var angkotBeroperasi = 6;
 
-for (let noAngkot = 1; noAngkot < jmlAngkot; noAngkot++) {
-    if( noAngkot <= angkotBeroperasi ) {
+for (let noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+    if( noAngkot <= angkotBeroperasi && noAngkot !== 5 ) {
         console.log(`Angkot no. ${noAngkot} beroperasi dengan baik`);
-    } else if( noAngkot === 8 ) {
+    } else if( noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
         console.log(`Angkot no. ${noAngkot} sedang lembur`);
     } else {
         console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi`);
